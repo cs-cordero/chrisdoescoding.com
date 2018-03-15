58f8f5800baa00abe9dddfed6988a4e6e87b9a0f
Christopher Cordero <chris.s.cordero@gmail.com>
2018-03-15 00:13:08 -0400

    overhaul views and html

--- chrisdoescoding/posts/views.py
+++ chrisdoescoding/posts/views.py
@@ -5,41 +5,34 @@ from django.http import HttpResponseRedirect
 from chrisdoescoding.posts.models import Post
 from chrisdoescoding.posts.utils import MarkdownParser
 
-import datetime
+from datetime import datetime
 
 
-class PostView(DetailView):
+class BasePostView(DetailView):
     model = Post
-    template_name = 'postview.html'
+    queryset = Post.objects.filter(publication_date__lte=datetime.utcnow())
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        if self.object.publication_date:
-            context = self.get_context_data(object=self.object)
-            return self.render_to_response(context)
-
-        return HttpResponseRedirect('/posts')
-
-
-class LatestPostView(DetailView):
-    model = Post
-    template_name = 'postview.html'
-
-    def get(self, request):
-        now = datetime.datetime.utcnow()
-        self.object = Post.objects.filter(publication_date__lte=now) \
-                                  .order_by('-publication_date')[0]
-        #TODO: Handle case where no object is returned
+        if not self.object:
+            raise Http404('Did not find a published post to return!')
         context = self.get_context_data(object=self.object)
         with MarkdownParser(self.object.body) as markdown:
             context.update({ 'markdown': markdown })
         return self.render_to_response(context)
 
+class LatestPostView(BasePostView):
+    template_name = 'latest_post_view.html'
+
+    def get_object(self):
+        return self.get_queryset().latest('publication_date')
+
+
+class PostView(BasePostView):
+    template_name = 'single_post_view.html'
+
 
 class AllPostsView(ListView):
-    template_name = 'listview.html'
+    template_name = 'all_posts_view.html'
     context_object_name = 'published_posts'
-
-    def get_queryset(self):
-        now = datetime.datetime.now()
-        return Post.objects.filter(publication_date__lte=now)
+    queryset = Post.objects.filter(publication_date__lte=datetime.utcnow())
