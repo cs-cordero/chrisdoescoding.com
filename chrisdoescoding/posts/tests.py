4f7af81b421cf476bf4e14411bd4060f7a3bcbf2
Christopher Cordero <chris.s.cordero@gmail.com>
2018-04-10 22:56:18 -0400

    added a minimum spec for tests

--- chrisdoescoding/posts/tests.py
+++ chrisdoescoding/posts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# latest post should show the latest by publication_date
+
+# latest post should skip any posts created most recent but not published
+
+# all posts should show only the published content
