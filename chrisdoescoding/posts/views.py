b68125d899e27002d8aa09576f9c684bddf5e3bc
Christopher Cordero <chris.s.cordero@gmail.com>
2018-04-10 22:52:53 -0400

    Incremental Updates

--- chrisdoescoding/posts/views.py
+++ chrisdoescoding/posts/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 
 from chrisdoescoding.posts.models import Post
 from chrisdoescoding.posts.utils import MarkdownParser
 
-from datetime import datetime
+from django.utils import timezone
 
 
 class BasePostView(DetailView):
     model = Post
-    queryset = Post.objects.filter(publication_date__lte=datetime.utcnow())
+    queryset = Post.objects.filter(publication_date__lte=timezone.now())
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -26,7 +26,10 @@ class LatestPostView(BasePostView):
     template_name = 'single_post_view.html'
 
     def get_object(self):
-        return self.get_queryset().latest('publication_date')
+        try:
+            return self.get_queryset().latest('publication_date')
+        except:
+            return None
 
 
 class PostView(BasePostView):
@@ -36,4 +39,4 @@ class PostView(BasePostView):
 class AllPostsView(ListView):
     template_name = 'all_posts_view.html'
     context_object_name = 'published_posts'
-    queryset = Post.objects.filter(publication_date__lte=datetime.utcnow())
+    queryset = Post.objects.filter(publication_date__lte=timezone.now())
--- config/settings/local.py
+++ config/settings/local.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'x1&6njoiwqnje!y63((#$y!o(y)@929&%!y31dftl0_ef(xrnz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
