567506db900b5cd827a359113da732ad8e21b6b0
Christopher Cordero <chris.s.cordero@gmail.com>
2018-04-19 22:16:46 -0400

    get the process working for nginx and gunicorn

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,6 +7,11 @@ BASE_DIR = os.path.abspath(os.path.join(__file__, '..', '..', '..'))
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'chrisdoescoding.com',
+    'www.chrisdoescoding.com'
 ]
 
 DATABASES = {
@@ -21,5 +26,3 @@ STATIC_URL = '/static/'
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
-
-INSTALLED_APPS = INSTALLED_APPS + ['mod_wsgi.server']
