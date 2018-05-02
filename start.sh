b03bfa48a58ec819d7c8bdbe74965a19e880cabf
Christopher Cordero <chris.s.cordero@gmail.com>
2018-05-02 00:06:26 -0400

    add devmode

--- start.sh
+++ start.sh
@@ -6,8 +6,9 @@ function help {
     echo "$0 [options]"
     echo
     echo "Options:"
-    echo "-h, --help                show brief help"
-    echo "-p, --prod                run in prod mode, with gunicorn"
+    echo "-h, --help        show brief help"
+    echo "-p, --prod        run in prod mode, with gunicorn"
+    echo "-d, --dev         run in dev mode, without gunicorn"
     echo
     echo
     exit 0
@@ -15,6 +16,7 @@ function help {
 
 # Parse Options
 PROD_MODE=0
+DEV_MODE=0
 while test $# -gt 0; do
     case "$1" in
         -h|--help)
@@ -22,10 +24,16 @@ while test $# -gt 0; do
             ;;
         -p|--prod)
             PROD_MODE=1
+            [[ $DEV_MODE = 1 ]] && help
+            shift
+            ;;
+        -d|--dev)
+            DEV_MODE=1
+            [[ $PROD_MODE = 1 ]] && help
             shift
             ;;
         *)
-            break
+            help
             ;;
     esac
 done
@@ -35,7 +43,10 @@ export PYTHONPATH=$ROOT:$ROOT/chrisdoescoding
 if [[ $PROD_MODE = 1 ]]; then
     export DJANGO_SETTINGS_MODULE='config.settings.prod'
     gunicorn chrisdoescoding.wsgi --bind=unix:/tmp/gunicorn.sock
-else
+elif [[ $DEV_MODE = 0 ]]; then
     export DJANGO_SETTINGS_MODULE='config.settings.local'
     gunicorn chrisdoescoding.wsgi
+else
+    export DJANGO_SETTINGS_MODULE='config.settings.local'
+    python chrisdoescoding/manage.py runserver
 fi
