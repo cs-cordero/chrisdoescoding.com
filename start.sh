47d0499192e804f7707b3e7a5830d86212e190c0
Christopher Cordero <chris.s.cordero@gmail.com>
2018-05-01 23:30:28 -0400

    bugfix: allow start.sh to run gunicorn

--- start.sh
+++ start.sh
@@ -1,6 +1,41 @@
 ROOT=$(dirname $0)
-echo $ROOT
+
+function help {
+    echo "$0 - start the server"
+    echo
+    echo "$0 [options]"
+    echo
+    echo "Options:"
+    echo "-h, --help                show brief help"
+    echo "-p, --prod                run in prod mode, with gunicorn"
+    echo
+    echo
+    exit 0
+}
+
+# Parse Options
+PROD_MODE=0
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            help
+            ;;
+        -p|--prod)
+            PROD_MODE=1
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
 
 export PYTHONPATH=$ROOT
-export DJANGO_SETTINGS_MODULE='config.settings.local'
-python $ROOT/chrisdoescoding/manage.py runserver
+if [[ $PROD_MODE = 1 ]]; then
+    export DJANGO_SETTINGS_MODULE='config.settings.prod'
+    gunicorn chrisdoescoding.wsgi
+else
+    export DJANGO_SETTINGS_MODULE='config.settings.local'
+    python $ROOT/chrisdoescoding/manage.py runserver
+fi
