e523af03dda1475f65ef86f8d4d072b785645f69
Christopher Cordero <chris.s.cordero@gmail.com>
2018-05-02 18:58:00 -0400

    add url to serve static files

--- chrisdoescoding/posts/urls.py
+++ chrisdoescoding/posts/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from chrisdoescoding.posts import views
@@ -7,4 +9,4 @@ urlpatterns = [
     path('<int:pk>/', views.PostView.as_view(), name='post'),
     path('random/', views.RandomPostView.as_view(), name='random'),
     path('', views.AllPostsView.as_view(), name='list'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
