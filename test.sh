534005e24e4ec2630cefe1d36eef82a6f75e1195
Christopher Cordero <chris.s.cordero@gmail.com>
2018-09-19 00:54:08 -0400

    include mypy in tests

--- test.sh
+++ test.sh
@@ -3,4 +3,10 @@ echo $ROOT
 
 export PYTHONPATH=$ROOT
 export DJANGO_SETTINGS_MODULE='config.settings.local'
+export MYPYPATH="$ROOT/chrisdoescoding/stubs"
+echo -ne "Running mypy tests..."\\r
+mypy --config-file $ROOT/chrisdoescoding/mypy.ini $ROOT/chrisdoescoding/posts
+[ ! $? = 0 ] && echo "Mypy test failed. Exiting..." && exit
+echo "Running mypy tests...Success!"
+echo
 python $ROOT/chrisdoescoding/manage.py test chrisdoescoding/
